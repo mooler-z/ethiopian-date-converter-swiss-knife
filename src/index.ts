e61fdcf981ed7e93a86ab41345b84bcba26803e9
jossephus <yosephtuemay64@gmail.com>
2024-12-29 19:14:55 +0300

    feat: use julian days conversion for gregorian to ethiopic conversion

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
  * A utility class for converting between Ethiopian and Gregorian calendar dates
  */
 
+import { jdnToEthiopic, gregorianToJDN } from "./utils";
+
 export class EthiopianCalendar {
   readonly weekDaysAmharic = ['እሁድ', 'ሰኞ', 'ማክ', 'ረቡ', 'ሐሙ', 'አርብ', 'ቅዳሜ'];
   readonly weekDaysTigrinya = [
@@ -193,62 +195,11 @@ export class EthiopianCalendar {
       throw new Error('Invalid Date between 5-14 May 1582.');
     }
 
-    const gregorianMonths = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const ethiopianMonths = [0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 5, 30, 30, 30, 30];
-
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-      gregorianMonths[2] = 29;
-    }
-
-    let ethiopianYear = year - 8;
-
-    if (ethiopianYear % 4 === 3) {
-      ethiopianMonths[10] = 6;
-    }
-
-    const newYearDay = this.startDayOfEthiopian(year - 8);
-
-    let until = 0;
-    for (let i = 1; i < gregorianMonth; i++) {
-      until += gregorianMonths[i];
-    }
-    until += gregorianDate;
-
-    let tahissas = ethiopianYear % 4 === 0 ? 26 : 25;
-
-    if (year < 1582) {
-      ethiopianMonths[1] = 0;
-      ethiopianMonths[2] = tahissas;
-    } else if (until <= 277 && year === 1582) {
-      ethiopianMonths[1] = 0;
-      ethiopianMonths[2] = tahissas;
-    } else {
-      tahissas = newYearDay - 3;
-      ethiopianMonths[1] = tahissas;
-    }
-
-    let m: number;
-    let ethiopianDate: number = 0;
-
-    for (m = 1; m < ethiopianMonths.length; m++) {
-      if (until <= ethiopianMonths[m]) {
-        ethiopianDate = (m === 1 || ethiopianMonths[m] === 0) ?
-          until + (30 - tahissas) : until;
-        break;
-      } else {
-        until -= ethiopianMonths[m];
-      }
-    }
-
-    if (m > 10) {
-      ethiopianYear += 1;
-    }
-
-    const order = [0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4];
-    const ethiopianMonth = order[m!];
-
     // Create date object from input date
     const dateObject = new Date(Date.UTC(inputs[0], inputs[1] - 1, inputs[2]));
+
+    let jdn = gregorianToJDN(year, gregorianMonth, gregorianDate);
+    const [ethiopianYear, ethiopianMonth, ethiopianDate] = jdnToEthiopic(jdn);
     
     // Format dates with dash separator
     const dmy = `${ethiopianDate}-${ethiopianMonth}-${ethiopianYear}`;
